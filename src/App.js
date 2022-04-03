dbc201fb0b9379d1a83552df271461aa0ada9e25
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-03 23:08:18 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+    <h1 className='text-6xl text-yellow-400'>Let's see that tailwind is working is not</h1>
     </div>
   );
 }
