34db5167482367ba1cd7c369738a4e96e4d29faa
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-03 22:59:21 +0600

    project created and react router, tailwind , rechart installed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
+import { BrowserRouter } from 'react-router-dom';
+const rootElement = document.getElementById("root");
+render(
+ 
+  <BrowserRouter>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </BrowserRouter>,
+  rootElement
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
