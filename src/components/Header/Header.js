5787e388b617b5a9685aac1abca6c214bb77c7f8
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-04 10:54:32 +0600

    home section done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
   return (
-    <div className=" bg-slate-100 p-6">
+    <div className=" bg-green-50	 p-6 mb-10">
      
 
       <nav className="  text-xl space-x-8 font-medium  ">
        
-        <Link to="/home">Home</Link>
-        <Link to="/reviews">Reviews</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/about">About</Link>
+        <NavLink className={({isActive}) => isActive ? 'text-orange-700':'text-black-700' } to="/home">Home</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'text-orange-700':'text-black-700'  } to="/reviews">Reviews</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'text-orange-700':'text-black-700'  } to="/dashboard">Dashboard</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'text-orange-700':'text-black-700'  } to="/blog">Blog</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'text-orange-700':'text-black-700'  } to="/about">About</NavLink>
       </nav>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import sd from '../../images/ee.png'
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <div className="grid lg:grid-cols-3 gap-9 lg:mb-10 mb-24">
+        <div className="lg:col-span-2  lg:ml-20 justify-center items-center flex flex-col lg:mt-20  ">
+         <h1 className="text-5xl font-extrabold  ">This MacBook Pro</h1>
+          <h1 className="text-5xl font-extrabold  text-fuchsia-500">Beyond Everything</h1>
+          <p className='text-lg px-6 py-10  lg:px-14 lg:py-8 text-justify'>This is the best ever macbook pro we have ever build. This machine comes with a M1 chipset which is more powerful than any devices we have ever built. With that machine you can do anything that you desire to do. Connect your 13-inch MacBook Pro to a power outlet using the included USB-C Charge Cable and 61W USB-C Power Adapter.
+
+Connect your 14-inch or 16-inch MacBook Pro to a power outlet using the included 67W USB-C Power Adapter, 96W USB-C Power Adapter, or 140W USB-C Power Adapter and USB-C to MagSafe 3 Cable.</p>
+          <button className="text-xl text-white font-semibold p-4 bg-indigo-500 rounded lg:mb-3 mb-12">
+            <Link to="/blog">Explore More</Link>
+          </button>
+        </div>
+        <div className="lg:col-span-1  mr-5 ">
+          <img className='h-96 lg:mt-32 ' src={ sd} alt="" />
+        </div>
+      </div>
+
+
     </div>
   );
 };
